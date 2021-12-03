739c690a23776177c8e62effb9856236a19ccca7
Gabbasov98 <marsel13_98@mail.ru>
2021-12-03 23:12:27 +0500

    about

--- about.php
+++ about.php
@@ -1,26 +1,172 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-  <?php include('head.php'); ?>
-  <title>О РЕСТОРАНЕ</title>
+	<?php include('head.php'); ?>
+	<title>О КОМПАНИИ</title>
 </head>
+
 <body>
-	
+
 	<div class="wrapper">
 		<div class="content">
 			<?php include('header.php'); ?>
+			<section class="company">
+				<div class="container">
+					<div class="company__inner">
+						<div class="company__left">
+							<img src="images/girl.jpg" alt="">
+						</div>
+						<div class="company__right">
+							<h2 class="company__title">Одноразовая посуда из экологических материалов мелким и крупным оптом</h2>
+							<p> <strong>Компания EKKO HVOEE</strong> является одним из основных розничных поставщиков одноразовой экологичной посуды и упаковочных форм. Продукция нашей компании которые позволяет сохранить эстетичный вид блюда, биохимический баланс и пищевую ценность разнообразных продуктов питания. <a href="">Представленный ассортимент столовых принадлежностей</a> изготавливается из сахарного тростника, бумаги, соломы злаковых и кукурузного крахмала.</p>
+							<p>Предлагаемый ассортимент посуды и упаковки не требует утилизации и всецело соответствует концепции здорового образа жизни и сохранения экобаланса окружающей среды.</p>
+							<div class="company__button">
+								<a href="" class="info__btn btn btn-green btn-rounded">
+									<span>ДОСТАВКА</span>
+								</a>
+								<a href="" class="info__btn btn btn-green btn-rounded">
+									<span>ВОПРОС-ОТВЕТ</span>
+								</a>
+								<a href="" class="info__btn btn btn-green btn-rounded">
+									<span>КОНТАКТЫ</span>
+								</a>
+							</div>
+							<div class="company__socials">
+								<span class="company__socials-title">FOLLOW US</span>
+								<div class="company__socials-link">
+									<a class="company__socials-item" href="">
+										<svg width="21" height="21" id="Слой_1" data-name="Слой 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 21.02 21.11">
+											<g id="ig_instagram_insta_photo" data-name="ig instagram insta photo">
+												<path fill="#fff" d="M301.88,418.52a.86.86,0,0,0-.5,1.11h0a3.42,3.42,0,1,1-2-2,.85.85,0,0,0,1.1-.49.86.86,0,0,0-.49-1.11,5.13,5.13,0,1,0,3,2.95.86.86,0,0,0-1.11-.49Z" transform="translate(-287.56 -410.27)"></path>
+												<circle fill="#fff" cx="16.49" cy="4.56" r="0.86"></circle>
+												<path fill="#fff" d="M308.34,414.82a4.29,4.29,0,0,0-4.2-4.28h0a65.76,65.76,0,0,0-12.07,0,4.29,4.29,0,0,0-4.28,4.21,80.91,80.91,0,0,0,0,12.06,4.29,4.29,0,0,0,4.2,4.28h0c2,.18,4.05.29,6.08.29s4-.1,6-.29a4.29,4.29,0,0,0,4.24-4.21A75.87,75.87,0,0,0,308.34,414.82Zm-1.71,12a2.57,2.57,0,0,1-2.57,2.56H304a60.66,60.66,0,0,1-11.9,0,2.55,2.55,0,0,1-2.56-2.56v-.07a79,79,0,0,1,0-11.92,2.56,2.56,0,0,1,2.56-2.57h.08q2.94-.27,5.91-.28t6,.28a2.55,2.55,0,0,1,2.57,2.56v.07A74.12,74.12,0,0,1,306.63,426.81Z" transform="translate(-287.56 -410.27)"></path>
+											</g>
+										</svg>
+										<span>Instagram</span>
+									</a>
+									<a class="company__socials-item" href="">
+										<svg width="10" height="17" id="Слой_1" data-name="Слой 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 9 16">
+											<path fill="#fff" d="M299.57,418.78h3v3h-3v7h-3v-7h-3v-3h3v-1.26a5.69,5.69,0,0,1,1.12-3.51,3.63,3.63,0,0,1,2.79-1.23h2.09v3h-2.1a.9.9,0,0,0-.9.9Z" transform="translate(-293.57 -412.78)"></path>
+										</svg>
+										<span>Facebook</span>
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="advantages__items">
+						<div class="advantages__item">
+							<div class="advantages__item-img">
+								<img src="images/advantage1.png" alt="">
+							</div>
+							<div class="advantages__item-title">Вся продукция производится в России из экологичных материалов</div>
+						</div>
+						<div class="advantages__item">
+							<div class="advantages__item-img">
+								<img src="images/advantage2.png" alt="">
+							</div>
+							<div class="advantages__item-title">Гарантия качества всей выпускаемой продукции</div>
+						</div>
+						<div class="advantages__item">
+							<div class="advantages__item-img">
+								<img src="images/advantage3.png" alt="">
+							</div>
+							<div class="advantages__item-title">Организуем доставку в любой порт мира</div>
+						</div>
+					</div>
+				</div>
+				<div class="company__gallery">
+					<a class="company__gallery-img" data-fancybox='gallery' href='images/svg/gallery1.jpg'>
+						<img src='images/svg/gallery1.jpg' alt=''>
+					</a>
+					<a data-fancybox='gallery' href='images/svg/gallery2.jpg'>
+						<img src='images/svg/gallery2.jpg' alt=''>
+					</a>
+					<a data-fancybox='gallery' href='images/svg/gallery6.jpg'>
+						<img src='images/svg/gallery6.jpg' alt=''>
+					</a>
+					<a data-fancybox='gallery' href='images/svg/gallery4.jpg'>
+						<img src='images/svg/gallery4.jpg' alt=''>
+					</a>
+					<a data-fancybox='gallery' href='images/svg/gallery5.jpg'>
+						<img src='images/svg/gallery5.jpg' alt=''>
+					</a>
+					<a data-fancybox='gallery' href='images/svg/gallery6.jpg'>
+						<img src='images/svg/gallery6.jpg' alt=''>
+					</a>
+					<a data-fancybox='gallery' href='images/svg/gallery7.jpg'>
+						<img src='images/svg/gallery7.jpg' alt=''>
+					</a>
 
-			
-
+				</div>
+			</section>
+			<!-- <section class="contact-about">
+				<div class="container">
+					<div class="contact-about__inner">
+						<span class="contact-about__title">КАК С НАМИ СВЯЗАТЬСЯ</span>
+						<div class="contact-about__items">
+							<div class="contact-about__address">
+								<span class="contact-about__adress-title">адрес компании</span>
+								<div class="contact-about__item">
+									<svg id="Слой_1" data-name="Слой 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 7.88 14">
+										<path fill="#bed681" d="M281.14,422.72a3.94,3.94,0,1,1-3.94-3.94A3.94,3.94,0,0,1,281.14,422.72Zm-3.61-2.19a.33.33,0,0,0-.33-.33,2.51,2.51,0,0,0-2.51,2.52.32.32,0,0,0,.32.33.33.33,0,0,0,.33-.33,1.86,1.86,0,0,1,1.86-1.86A.33.33,0,0,0,277.53,420.53Zm-.33,7a5.14,5.14,0,0,0,.88-.08v4.28l-.61.91a.33.33,0,0,1-.54,0l-.6-.91v-4.28A4.94,4.94,0,0,0,277.2,427.53Z" transform="translate(-273.26 -418.78)"></path>
+									</svg>
+									<a href="" class="contact-about__text">Россия, г. Владивосток, ул. Лазо 28, оф 19</a>
+								</div>
+							</div>
+							<div class="contact-about__mail">
+								<span class="contact-about__mail-title">эл.почта</span>
+								<div class="contact-about__item">
+									<svg id="Слой_1" data-name="Слой 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 12 12">
+										<path fill="#bed681" d="M283.2,424.53v6.15a1.13,1.13,0,0,1-1.12,1.13h-9.75a1.13,1.13,0,0,1-1.13-1.13v-6.15a1.13,1.13,0,0,1,.41-.87l3.17-2.54a2.89,2.89,0,0,1,4.84,0c.55.43,3.06,2.45,3.17,2.54A1.13,1.13,0,0,1,283.2,424.53Zm-1.12.07a.11.11,0,0,0-.06-.11l-3.1-2.5c-.41-.32-1.16-1.06-1.72-1.06s-1.29.73-1.72,1.06l-3.1,2.5s-.05.06-.05.11v5.94a.14.14,0,0,0,.14.14h9.47a.14.14,0,0,0,.14-.14Zm-.79,1.94c-.68.54-1.39,1.12-1.67,1.33a4.26,4.26,0,0,1-2.42,1.31,4.31,4.31,0,0,1-2.42-1.31l-1.66-1.33a.28.28,0,0,1,0-.4l.35-.43a.29.29,0,0,1,.4,0l1.65,1.32c.41.33,1.17,1.07,1.72,1.07s1.3-.73,1.72-1.07l1.66-1.32a.27.27,0,0,1,.39,0l.36.43A.29.29,0,0,1,281.29,426.54Z" transform="translate(-271.2 -419.81)"></path>
+									</svg>
+									<a href="" class="contact-about__text">hello@hvoee.com</a>
+								</div>
+							</div>
+							<div class="contact-about__telephone">
+								<span class="contact-about__telephone-title">телефоны</span>
+								<div class="contact-about__item">
+									<svg id="Слой_1" data-name="Слой 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 7.5 12">
+										<path fill="#bed681" d="M281,420.93v9.75a1.13,1.13,0,0,1-1.12,1.13h-5.25a1.13,1.13,0,0,1-1.13-1.13v-9.75a1.12,1.12,0,0,1,1.13-1.12h5.25A1.12,1.12,0,0,1,281,420.93Zm-1.12.28a.29.29,0,0,0-.29-.28h-4.68a.28.28,0,0,0-.28.28v7.32a.28.28,0,0,0,.28.28h4.68a.29.29,0,0,0,.29-.28Zm-1.88,9.1a.75.75,0,0,0-1.5,0,.75.75,0,0,0,1.5,0Z" transform="translate(-273.45 -419.81)"></path>
+									</svg>
+									<a href="" class="contact-about__number">8 (423) 240 08 65</a>
+								</div>
+								<div class="contact-about__item">
+									<a href="79147012510" class="contact-about__number">+7 914 701 2510</a>
+									<a href="" class="contact-about__network">Tel / WhatsApp / WeChat</a>
+								</div>
+							</div>
+							<div class="contact-about__social">
+								<div class="contact-about__social-image">
+									<svg width="21" height="21" id="Слой_1" data-name="Слой 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 21.02 21.11">
+										<g id="ig_instagram_insta_photo" data-name="ig instagram insta photo">
+											<path fill="#fff" d="M301.88,418.52a.86.86,0,0,0-.5,1.11h0a3.42,3.42,0,1,1-2-2,.85.85,0,0,0,1.1-.49.86.86,0,0,0-.49-1.11,5.13,5.13,0,1,0,3,2.95.86.86,0,0,0-1.11-.49Z" transform="translate(-287.56 -410.27)"></path>
+											<circle fill="#fff" cx="16.49" cy="4.56" r="0.86"></circle>
+											<path fill="#fff" d="M308.34,414.82a4.29,4.29,0,0,0-4.2-4.28h0a65.76,65.76,0,0,0-12.07,0,4.29,4.29,0,0,0-4.28,4.21,80.91,80.91,0,0,0,0,12.06,4.29,4.29,0,0,0,4.2,4.28h0c2,.18,4.05.29,6.08.29s4-.1,6-.29a4.29,4.29,0,0,0,4.24-4.21A75.87,75.87,0,0,0,308.34,414.82Zm-1.71,12a2.57,2.57,0,0,1-2.57,2.56H304a60.66,60.66,0,0,1-11.9,0,2.55,2.55,0,0,1-2.56-2.56v-.07a79,79,0,0,1,0-11.92,2.56,2.56,0,0,1,2.56-2.57h.08q2.94-.27,5.91-.28t6,.28a2.55,2.55,0,0,1,2.57,2.56v.07A74.12,74.12,0,0,1,306.63,426.81Z" transform="translate(-287.56 -410.27)"></path>
+										</g>
+									</svg>
+								</div>
+								<div class="contact-about__social-image">
+									<svg width="10" height="17" id="Слой_1" data-name="Слой 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 9 16">
+										<path fill="#fff" d="M299.57,418.78h3v3h-3v7h-3v-7h-3v-3h3v-1.26a5.69,5.69,0,0,1,1.12-3.51,3.63,3.63,0,0,1,2.79-1.23h2.09v3h-2.1a.9.9,0,0,0-.9.9Z" transform="translate(-293.57 -412.78)"></path>
+									</svg>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section> -->
 		</div>
-		
-	<?php include('footer.php'); ?>
+
+		<?php include('footer.php'); ?>
 	</div>
 
 
 
-	
-  <?php include('modal.php'); ?>
+
+	<?php include('modal.php'); ?>
 
 </body>
+
 </html>
