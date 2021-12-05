0a3997a76546a6bc43338bff2510b489bd05cb41
Marsel Gabbasov <marsel13_98@mail.ru>
2021-12-05 15:09:35 +0300

    edit

--- head.php
+++ head.php
@@ -1,7 +1,5 @@
   <meta charset="UTF-8">
-  <!-- <title>Document</title>   -->
   <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
-  <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css"> -->
   <link rel="stylesheet" href="css/bootstrap.min.css">
   <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
   <link rel="stylesheet" href="css/nice-select.css">
